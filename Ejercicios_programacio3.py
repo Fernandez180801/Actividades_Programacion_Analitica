793af23590cdf6dfe0bd329e4665251f56f3141e
Fernandez180801 <bsofia180801@gmail.com>
2026-09-11 19:07:02 -0500

    Add files via upload

---
+++ Ejercicios_programacio3.py
@@ -0,0 +1,41 @@
+# %% Definir una función
+def saludar():
+ print("Hola")
+saludar()
+
+# %% Llamar a la función
+def bienvenida():
+ print("Bienvenido al curso")
+bienvenida()
+
+# %% Predice el orden de ejecución 
+uno():
+ print("A")
+print("B")
+uno()
+print("C"
+      
+# %% Corrige el orden
+def saludar(nombre):
+ print("Hola,", nombre)
+
+saludar("Ana")
+# %% Función con un parámetro 
+def saludar(nombre):
+ print("Hola,", nombre)
+saludar("Ana")
+# %% Función con dos parámetros 
+def area(base, area):
+ return base * altura
+print(area(3, 4))
+
+# %% Completa la llamada
+def area(base, altura):
+ return base * altura
+print(area(5.0, 5.0))
+# %% Reutilizar la misma función
+def con_iva(precio):
+ return precio * 1.19
+print(con_iva(___))
+print(con_iva(___))
+print(con_iva(___))
